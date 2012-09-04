babfb2dc21bf2de76e8936bebf45562f1f9a27fa
simsicon <simsicon@gmail.com>
2012-09-04 15:01:10 +0800

    Commit

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,5 @@
+class AuthController < ApplicationController
+  def callback
+    
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loud::Application.config.secret_token = '2cea1b719ef60092fdecbc33824cc9e0c6dd43dc1174cdb544fe4ef8df3e302d90863a94864ddf864bcca08c08c92708407abb40f0abd85c8b8822d4a60bb8ac'
